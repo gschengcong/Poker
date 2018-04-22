57c967cc5287c42174a92f6d5b5c34e834c60fe2
Cong Cheng <cong@Congs-MacBook-Pro.local>
2018-04-22 00:03:51 -0700

    finish card_tool

--- poker/Game/card_tool.py
+++ poker/Game/card_tool.py
@@ -7,6 +7,8 @@
 # local game_settings = require 'Settings.game_settings'
 # local arguments = require 'Settings.arguments'
 # local constants = require 'Settings.constants'
+import random
+
 from Settings import game_settings
 import math
 
@@ -59,97 +61,98 @@ class CardTool(object):
             out[i] = out[i]/(total + 0.0)
         return out
 
+    # --- Randomly samples a range vector which is valid with a given board.
+    # -- @param board a possibly empty vector of board cards
+    # -- @param[opt] seed a seed for the random number generator
+    # -- @return a range vector where invalid hands are given 0 probability, each
+    # -- valid hand is given a probability randomly sampled from the uniform
+    # -- distribution on [0,1), and the resulting range is normalized
+    def get_random_range(self, board):
+        out = self.get_uniform_range(board)
+        total = 0.0
+        for i in range(0, len(out)):
+            if out[i] != 0:
+                out[i] += random.random()
+                total += out[i]
+        for i in range(0, len(out)):
+            out[i] = out[i]/total
+        return out
 
+    # --- Checks if a range vector is valid with a given board.
+    # -- @param range a range vector to check
+    # -- @param board a possibly empty vector of board cards
+    # -- @return `true` if the range puts 0 probability on invalid hands and has
+    # -- total probability 1
+    def is_valid_range(self, rangeVector, board):
+        possibleHand = self.get_possible_hand_indexes(board)
+        totalProb = 0.0
+        for i in range(0, len(rangeVector)):
+            if rangeVector[i] != 0 and possibleHand[i] == 0:
+                return False
+            totalProb += rangeVector[i]
+        return math.fabs(1-totalProb) < 0.0001
 
+    # --- Gives the current betting round based on a board vector.
+    # -- @param board a possibly empty vector of board cards
+    # -- @return the current betting round
+    def board_to_street(self, board):
+        if(len(board) == 0):
+            return 1
+        return 2
+
+    # --- Gives the number of possible boards.
+    # -- @return the number of possible boards
+    def get_boards_count(self):
+        if self.m['board_card_count'] == 1:
+            return self.m['card_count']
+        elif self.m['board_card_count'] == 2:
+            return self.m['card_count'] * (self.m['card_count'] - 1) /2
+        return self.m['card_count']
+
+    # --- Gives all possible sets of board cards for the game.
+    # -- @return an NxK tensor, where N is the number of possible boards, and K is
+    # -- the number of cards on each board
+    # only support 1 card for now
+    def get_second_round_boards(self):
+        out = [0] * self.m['card_count']
+        if self.m['board_card_count'] == 1:
+            for card in range(0, len(out)):
+                out[card] = card
+        return out
+
+    # def _init_board_index_table(self):
+
+    # --- Normalizes a range vector over hands which are valid with a given board.
+    # -- @param board a possibly empty vector of board cards
+    # -- @param range a range vector
+    # -- @return a modified version of `range` where each invalid hand is given 0
+    # -- probability and the vector is normalized
+    def normalize_range(self, board, rangeVector):
+        possibleHand = self.get_possible_hand_indexes(board)
+        total = 0.0
+        for i in range(0, len(rangeVector)):
+            if(possibleHand[i] == 0):
+                rangeVector[i] = 0
+            total += rangeVector[i]
+        for i in range(0, len(rangeVector)):
+            rangeVector[i] = rangeVector[i] / total
+        return rangeVector
 
 
 cardTool = CardTool()
 print(cardTool.hand_is_possible([3]))
 print(cardTool.get_possible_hand_indexes([2,3]))
 print(cardTool.get_impossible_hand_indexes([3]))
-print(cardTool.get_uniform_range([]))
+print(cardTool.get_uniform_range([1]))
+print(cardTool.get_random_range([0]))
+print(cardTool.is_valid_range([0.1, 0.2, 0.3, 0.0, 0.1, 0.3], [3]))
+print(cardTool.board_to_street([1]))
+print(cardTool.get_boards_count())
+print(cardTool.get_second_round_boards())
+print(cardTool.normalize_range([1], [0.1, 0.5, 0.23, 0.6, 0.0, 1]))
 
-# --- Randomly samples a range vector which is valid with a given board.
-# -- @param board a possibly empty vector of board cards
-# -- @param[opt] seed a seed for the random number generator
-# -- @return a range vector where invalid hands are given 0 probability, each
-# -- valid hand is given a probability randomly sampled from the uniform
-# -- distribution on [0,1), and the resulting range is normalized
-# function M:get_random_range(board, seed)
-#   seed = seed or torch.random()
-#
-#   local gen = torch.Generator()
-#   torch.manualSeed(gen, seed)
-#
-#   local out = torch.rand(gen, game_settings.card_count):typeAs(arguments.Tensor())
-#   out:cmul(self:get_possible_hand_indexes(board))
-#   out:div(out:sum())
-#
-#   return out
-# end
-#
-# --- Checks if a range vector is valid with a given board.
-# -- @param range a range vector to check
-# -- @param board a possibly empty vector of board cards
-# -- @return `true` if the range puts 0 probability on invalid hands and has
-# -- total probability 1
-# function M:is_valid_range(range, board)
-#   local check = range:clone()
-#   local only_possible_hands = range:clone():cmul(self:get_impossible_hand_indexes(board)):sum() == 0
-#   local sums_to_one = math.abs(1.0 - range:sum()) < 0.0001
-#   return only_possible_hands and sums_to_one
-# end
-#
-# --- Gives the current betting round based on a board vector.
-# -- @param board a possibly empty vector of board cards
-# -- @return the current betting round
-# function M:board_to_street(board)
-#   if board:dim() == 0 then
-#     return 1
-#   else
-#     return 2
-#   end
-# end
-#
-# --- Gives all possible sets of board cards for the game.
-# -- @return an NxK tensor, where N is the number of possible boards, and K is
-# -- the number of cards on each board
-# function M:get_second_round_boards()
-#   local boards_count = self:get_boards_count()
-#   if game_settings.board_card_count == 1 then
-#     local out = arguments.Tensor(boards_count, 1)
-#     for card = 1, game_settings.card_count do
-#       out[{card, 1}] = card
-#     end
-#     return out
-#   elseif game_settings.board_card_count == 2 then
-#     local out = arguments.Tensor(boards_count, 2)
-#     local board_idx = 0;
-#     for card_1 = 1, game_settings.card_count do
-#       for card_2 = card_1 + 1, game_settings.card_count do
-#         board_idx = board_idx + 1
-#         out[{board_idx, 1}] = card_1
-#         out[{board_idx, 2}] = card_2
-#       end
-#     end
-#     assert(board_idx == boards_count, 'wrong boards count!')
-#     return out
-#   else
-#     assert(false, 'unsupported board size' )
-#   end
-# end
-#
-# --- Gives the number of possible boards.
-# -- @return the number of possible boards
-# function M:get_boards_count()
-#   if game_settings.board_card_count == 1 then
-#     return game_settings.card_count
-#   elseif game_settings.board_card_count == 2 then
-#     return (game_settings.card_count * (game_settings.card_count - 1)) / 2
-#   else
-#     assert(false, 'unsupported board size' )
-#   end
-# end
+
+
 #
 # --- Initializes the board index table.
 # -- @local
@@ -185,21 +188,7 @@ print(cardTool.get_uniform_range([]))
 #   return index
 # end
 #
-# --- Normalizes a range vector over hands which are valid with a given board.
-# -- @param board a possibly empty vector of board cards
-# -- @param range a range vector
-# -- @return a modified version of `range` where each invalid hand is given 0
-# -- probability and the vector is normalized
-# function M:normalize_range(board, range)
-#   local mask = self:get_possible_hand_indexes(board)
-#   local out = range:clone():cmul(mask)
-#   --return zero range if it all collides with board (avoid div by zero)
-#   if out:sum() == 0 then
-#     return out
-#   end
-#   out:div(out:sum())
-#   return out
-# end
+
 #
 #
 # return M
